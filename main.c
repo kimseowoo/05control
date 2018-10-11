e050092e8ecbd2b2424d2c0d93d5c32477e94301
kimseowoo <swoo422@naver.com>
2018-10-11 15:27:30 +0900

    05control

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	
+	printf("Input an integer : ");
+	scanf("%d" , &i);
+	
+	
+	if(i>0)
+	{
+	
+		printf("양수입니다.\n");
+ 	}
+	else if(i<0) 
+	{
+		
+		printf("음수입니다.\n");
+	}
+	else
+	{
+		printf("0");
+	}
+	return 0;
+}
